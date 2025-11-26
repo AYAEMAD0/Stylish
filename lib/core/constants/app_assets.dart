1266628a00a68bac47de841f7a9ba9719ff55c9c
Aya Emad <ayaelbaroudy0@gmail.com>
2025-11-26 13:07:24 +0200

    + add icons in asset and file appAsset

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,3 +1,20 @@
 class AppAssets{
   static const appLogo='assets/images/svgs/logo.svg';
+  //icon
+  static const arrowIcon='assets/images/svgs/arrow.svg';
+  static const cartIcon='assets/images/svgs/cart.svg';
+  static const cartShoppingIcon='assets/images/svgs/cart_shopping.svg';
+  static const categorySelectedIcon='assets/images/svgs/category_selected.svg';
+  static const categoryUnselectedIcon='assets/images/svgs/category_unselected.svg';
+  static const deleteIcon='assets/images/svgs/delete.svg';
+  static const editIcon='assets/images/svgs/edit.svg';
+  static const favSelectedIcon='assets/images/svgs/fav_selected.svg';
+  static const favUnselectedIcon='assets/images/svgs/fav_unselected.svg';
+  static const homeSelectedIcon='assets/images/svgs/home_selected.svg';
+  static const homeUnselectedIcon='assets/images/svgs/home_unselected.svg';
+  static const searchIcon='assets/images/svgs/search.svg';
+  static const starIcon='assets/images/svgs/star.svg';
+  static const userSelectedIcon='assets/images/svgs/user_selected.svg';
+  static const userUnselectedIcon='assets/images/svgs/user_unselected.svg';
+
 }
